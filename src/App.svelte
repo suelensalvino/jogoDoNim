bd607554a314234722b72131273dbaa562abd9c7
Suelen Salvino <suelensalvinocs@gmail.com>
2022-07-01 09:35:55 -0300

    Adicionando base do projeto

---
+++ src/App.svelte
@@ -0,0 +1,21 @@
+<script>
+	import Jogo from './Jogar.svelte'
+	import ComoJogar from './Como-jogar.svelte'
+	import Sobre from './Sobre.svelte'
+	import Ajuda from './Ajuda.svelte'
+	import Menu from './Menu.svelte'
+
+	import { estado } from './Estado.js'
+</script>
+
+{#if $estado === 'menu'}
+	<Menu/>
+{:else if $estado === 'jogar'}
+	<Jogo/>
+{:else if $estado === 'como-jogar'}
+	<ComoJogar/>
+{:else if $estado === 'sobre'}
+	<Sobre/>
+{:else if $estado === 'ajuda'}
+	<Ajuda/>
+{/if}
---
+++ src/Menu.svelte
@@ -0,0 +1,24 @@
+<script>
+	import { estado } from './Estado.js'
+	import { trocarEstadoDoJogo } from './Estado.js'
+</script>
+
+<h1>
+	NIM
+</h1>
+
+<div class='menu' on:click={() => trocarEstadoDoJogo('jogar')}>
+	Jogar
+</div>
+
+<div class='menu' on:click={() => trocarEstadoDoJogo('como-jogar')}>
+	Como jogar
+</div>
+
+<div class='menu' on:click={() => trocarEstadoDoJogo('ajuda')}>
+	Ajuda
+</div>
+
+<div class='menu' on:click={() => trocarEstadoDoJogo('sobre')}>
+	Sobre
+</div>
