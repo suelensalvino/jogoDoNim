f67531a2c9fd6146e0d9de02716a942128b4cbc4
Liliane Alves <108375947+LiliAlves@users.noreply.github.com>
2022-07-04 18:33:58 -0300

    https://github.com/suelensalvino/jogoDoNim.git
    Essa versão do jogo Nim foi desenvolvida pelos estudantes Davi Luna, Liliane Alves, Maria Eduarda Vieira, Samuel Vanderlei e Suelen Salviano, todos  do 1º período do curso de Tecnologia e Sistemas para Internet na disciplina de Lógica de Programação sob a orientação do professor Allan Diego Lima.

---
+++ src/Ajuda.svelte
@@ -0,0 +1,2 @@
+//Legenda para a aba Sobre
+Essa versão do jogo Nim foi desenvolvida pelos estudantes Davi Luna, Liliane Alves, Maria Eduarda Vieira, Samuel Vanderlei e Suelen Salviano, todos  do 1º período do curso de Tecnologia e Sistemas para Internet na disciplina de Lógica de Programação sob a orientação do professor Allan Diego Lima. 
