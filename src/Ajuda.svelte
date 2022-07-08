8f2086a0560b671b5ff382b82c54060d03100140
Liliane Alves <108375947+LiliAlves@users.noreply.github.com>
2022-07-07 22:45:06 -0300

    Update Ajuda.svelte

--- src/Ajuda.svelte
+++ src/Ajuda.svelte
@@ -1,2 +1,9 @@
-//Legenda para a aba Sobre
-Essa versão do jogo Nim foi desenvolvida pelos estudantes Davi Luna, Liliane Alves, Maria Eduarda Vieira, Samuel Vanderlei e Suelen Salviano, todos  do 1º período do curso de Tecnologia e Sistemas para Internet na disciplina de Lógica de Programação sob a orientação do professor Allan Diego Lima. 
+<script>
+	import { estado } from './Estado.js'
+	import { trocarEstadoDoJogo } from './Estado.js'
+</script>
+<h1>Como Jogar</h1>
+<p>O jogo NIM foi pensado para dois jogadores que podem ser duas pessoas ou uma pessoa e uma máquina. Neste caso, o jogo será executado por uma pessoa e uma máquina que irão retirar as peças dispostas em algumas colunas. 
+  Cada jogador realiza jogadas alternadas. Um movimento consiste em retirar algumas peças de uma única coluna; cabe a cada jogador decidir o número de peças a serem  removidas.
+  Para eliminar as peças, é necessário deslocar o mouse sobre a quantidade desejada, clicando quando você quiser removê-las, a quantidade selecionada deve mudar de cor indicando que mais uma jogada foi executada. O jogo acaba quando não houver mais peças disponíveis e o jogador que fez a última jogada, vence.
+  </p>
