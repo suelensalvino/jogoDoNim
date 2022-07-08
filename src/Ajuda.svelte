70a4dfab4c0932d9a4ad738f04cabd3d1ce64791
suelensalvino <suelensalvinocs@gmail.com>
2022-07-08 17:23:30 -0300

    Adicionando mais conteúdo a página de ajuda

--- src/Ajuda.svelte
+++ src/Ajuda.svelte
@@ -1,9 +1,29 @@
+<svelte:head>
+	<link rel="stylesheet" href="/style/ajuda.css">
+</svelte:head>
+
 <script>
 	import { estado } from './Estado.js'
 	import { trocarEstadoDoJogo } from './Estado.js'
 </script>
-<h1>Como Jogar</h1>
-<p>O jogo NIM foi pensado para dois jogadores que podem ser duas pessoas ou uma pessoa e uma máquina. Neste caso, o jogo será executado por uma pessoa e uma máquina que irão retirar as peças dispostas em algumas colunas. 
-  Cada jogador realiza jogadas alternadas. Um movimento consiste em retirar algumas peças de uma única coluna; cabe a cada jogador decidir o número de peças a serem  removidas.
-  Para eliminar as peças, é necessário deslocar o mouse sobre a quantidade desejada, clicando quando você quiser removê-las, a quantidade selecionada deve mudar de cor indicando que mais uma jogada foi executada. O jogo acaba quando não houver mais peças disponíveis e o jogador que fez a última jogada, vence.
-  </p>
+
+<h1>Dúvidas sobre o jogo</h1>
+
+<p>NÃO SABE COMO FUNCIONA O JOGO?</p>
+<p>O jogo NIM foi pensado para dois jogadores que podem ser duas pessoas ou uma pessoa e uma máquina.
+  Cada jogador realiza jogadas alternadas. Um movimento consiste em retirar algumas peças de uma única coluna; 
+  cabe a cada jogador decidir o número de peças a serem  removidas.
+  Para eliminar as peças, é necessário deslocar o mouse sobre a quantidade desejada, clicando quando você quiser removê-las, a quantidade selecionada deve mudar de cor indicando que mais uma jogada foi executada. 
+  O jogo acaba quando não houver mais peças disponíveis e o jogador que fez a última jogada, vence.
+</p>
+
+<h1>Problemas técnicos</h1>
+<p>PROBLEMAS DE CONEXÃO, PROBLEMAS COM O SERVIDOR, JOGO TRAVANDO, ETC.</p>
+<p>Algumas vezes o jogo pode ficar travado em uma tela. 
+  Isso geralmente está ligado a uma necessidade de atualizar as informações do jogo e por isso acaba ocorrendo esse problema.
+  Para solucioná-lo, verifique sua conexão com a internet e/ou atualize a página apertando a tecla F5.
+</p>
+
+<h1>Suporte</h1>
+<p>Se você precisa de ajuda, seja com problemas técnicos, dificuldades para jogar e etc, por favor, fale com o suporte através do e-mail:</p>
+<p>suporte-jogonim@gmail.com</p>
