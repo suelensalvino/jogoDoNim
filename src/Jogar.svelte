0fce6a3c8141410084e1ba8bd690752677c8f1e6
Davi Luna <86129094+davifurao@users.noreply.github.com>
2022-07-11 03:27:20 -0300

    adicionado as opções de parada
    adicionado a tag body(para o css funcionar) pois eu tinha esquecido.

--- src/Jogar.svelte
+++ src/Jogar.svelte
@@ -3,45 +3,98 @@
 </svelte:head>
 <script>
 	
-
 	
 	let tabela= ['','','',''];
 	let tabela1= ['','',''];
 	let tabela2= ['',''];
 	let tabela3= [''];
 	
+	var contador=0;
 	
 
-
-
+	
+	function parada(contador){
+		if (contador==10){
+			 return 'Parabéns!!!Você ganhou'
+		}else{
+			return 'Bom jogo'
+		}
+	}
+	
 	function remover(i){
     tabela = tabela.slice(0,-1)
-		console.log(tabela)
+		console.log(tabela);
 		
-		
+		t()
+		console.log(contador)
   }
+	
+	function t(tabela){
+		if(tabela=[]){
+			contador++
+		}
+	}
 	
 	function remover1(i){
     tabela1 = tabela1.slice(0,-1)
-		console.log(tabela1)
-		
+		console.log(tabela1);
+		t1()
+		console.log(contador)
 	
   }
+	
+	function t1(tabela1){
+		if(tabela1=[]){
+			contador++
+		}
+	}
 	
 	function remover2(i){
     tabela2 = tabela2.slice(0,-1)
-		console.log(tabela2)
-
+		console.log(tabela2);
+		t2()
+	console.log(contador)
 		
   }
+	function t2(tabela2){
+		if(tabela2=[]){
+			contador++ 
+		}
+	}
+	
 	function remover3(i){
     tabela3 = tabela3.slice(0,-1)
-		console.log(tabela3)
+		console.log(tabela3);
+	t3()
+		console.log(contador)
 	
   }
+	function t3(tabela3){
+		if (tabela3=[]){
+			contador ++
+		}
+	}
+
+	
 	
 let link= '/public/images/moeda.png'
 </script>
+
+
+
+
+
+
+<body>
+	
+
+
+<h1>
+	{parada(contador)}
+	</h1>
+
+
+
 
 <table>
 	
@@ -77,3 +130,5 @@ let link= '/public/images/moeda.png'
 	
 	
 </table>
+
+	</body>
