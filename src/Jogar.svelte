5be76c3aae8c399a10c5a7cba44c9f8cef228671
Suelen Salvino <suelensalvinocs@gmail.com>
2022-07-12 16:08:34 -0300

    Atualizando funções de jogada e ganhador do jogo

--- src/Jogar.svelte
+++ src/Jogar.svelte
@@ -1,169 +1,140 @@
-<svelte:head>
-	<link rel="stylesheet" href="/style/jogar.css">
-</svelte:head>
 <script>
+	import VoltarMenu from "./VoltarMenu.svelte";
 
-import VoltarMenu from "./VoltarMenu.svelte";
-	
-	
-	let tabela= ['','','',''];
-	let tabela1= ['','','']; 
-	let tabela2= ['',''];
-	let tabela3= ['']; 
-	var jogador1='Player 1';
-	var jogador2='Player 2';
-	var contador=0;
-	
-	var jogador=jogador1//aqui onde pode trocar na tela o nome do player antes de começar o jogo
-	
-	function botaojogador2(){
-		return jogador=jogador2
+	let tabela = ["", "", "", ""];
+	let tabela1 = ["", "", ""];
+	let tabela2 = ["", ""];
+	let tabela3 = [""];
+	var jogador1 = "Player 1";
+	var jogador2 = "Player 2";
+	var contador = 0;
+	var ganhador = 0;
+
+	var jogador = jogador1; //aqui onde pode trocar na tela o nome do player antes de começar o jogo
+
+	function botaojogador2() {
+		return (jogador = jogador2);
 	}
-	function botaojogador1(){
-		return jogador=jogador1
+	function botaojogador1() {
+		return (jogador = jogador1);
 	}
 
-	
-	function parada(contador){
-		if (contador==10){//vencedor
-			 return 'Parabéns!!!Você ganhou!!'
-			 
-		}else if(contador==9){//ultima pessoa a jogar
-			if(jogador=jogador1){ jogador=jogador2; return'Talvez numa proxima vez...'}//mostra na caixa do textoessa msg com o jogador 2(o que foi o ultimo a clicar)
-			else if(jogador=jogador2){jogador=jogador1;return'Talvez numa próxima vez'}//mostra na caixa do textoessa msg com o jogador 1(o que foi o ultimo a clicar)
-	
-		}else if(contador==0){
-			return 'START!'
-		}
-		else{//jogo em si
-			return 'Sua Vez.' 
+	function jogada() {
+		if (jogador == jogador1 && contador == 3) {
+			contador = 0;
+			jogador = jogador2;
+		} else if (jogador == jogador2 && contador == 3) {
+			contador = 0;
+			jogador = jogador1;
+		} else {
+			return "	";
 		}
 	}
-	
-	function remover(i){
-    tabela = tabela.slice(0,-1)
-		console.log(tabela);
-		
-		t()
-		console.log(contador)
-  }
-	
-	function t(tabela){
-		if(tabela=[]){
-			contador++
-			
+
+	function parada(ganhador) {
+		if (ganhador == 0) {
+			return "START";
+		} else if (ganhador == 10 && jogador == jogador1) {
+			return "PARABÉNS! VOCÊ VENCEU";
+		} else if (ganhador == 10 && jogador == jogador2) {
+			return "PARABÉNS! VOCÊ VENCEU";
+		} else {
+			return "Que vença o melhor";
 		}
 	}
-	
-	function remover1(i){
-    tabela1 = tabela1.slice(0,-1)
-		console.log(tabela1);
-		t1()
-		console.log(contador)
-	
-  }
-	
-	function t1(tabela1){
-		if(tabela1=[]){
-			contador++
-		}
+
+	function remover(i) {
+		tabela = tabela.slice(0, -1);
+		contador++;
+		jogada();
+		parada();
+		ganhador++;
 	}
-	
-	function remover2(i){
-    tabela2 = tabela2.slice(0,-1)
-		console.log(tabela2);
-		t2()
-	console.log(contador)
-	
-  }
-	function t2(tabela2){
-		if(tabela2=[]){
-			contador++ 
-		}
+
+	function remover1(i) {
+		tabela1 = tabela1.slice(0, -1);
+		contador++;
+		jogada();
+		parada();
+		ganhador++;
 	}
-	
-	function remover3(i){
-    tabela3 = tabela3.slice(0,-1)
-		console.log(tabela3);
-	t3()
-		console.log(contador)
-		
-  }
-	function t3(tabela3){
-		if (tabela3=[]){
-			contador ++
-		}
+
+	function remover2(i) {
+		tabela2 = tabela2.slice(0, -1);
+		contador++;
+		jogada();
+		parada();
+		ganhador++;
 	}
-	
-	
-	
-	
-	
-let link= '/public/images/moeda.png' 
+
+	function remover3(i) {
+		tabela3 = tabela3.slice(0, -1);
+		contador++;
+		jogada();
+		parada();
+		ganhador++;
+	}
+
+	let link = "/public/images/moeda.png";
 </script>
 
-
-
-
-
+<svelte:head>
+	<link rel="stylesheet" href="/style/jogar.css" />
+</svelte:head>
 
 <body>
-	
-
-
 	<h1 class="start">
-		{parada(contador)}
+		{parada(ganhador)}
 	</h1>
 	<h1 class="player">
 		{jogador}
+		{jogada()}
 	</h1>
 
-
-
-
-
-<table>
-	
+	<table>
 		<tr>
-			
-				<td on:click={() => remover(tabela)}>
-				{#each tabela as tabela,i}{tabela}<img src={link} alt='yes'>{/each} 
-				</td>
+			<td on:click={() => remover(tabela)}>
+				{#each tabela as tabela, i}{tabela}<img
+						src={link}
+						alt="yes"
+					/>{/each}
+			</td>
 		</tr>
-	
-	<tr>
-			
-				<td on:click={() => remover1(tabela1)}>
-				{#each tabela1 as tabela1,i}{tabela1}<img src={link} alt='yes'>{/each} 
-				</td>
-		</tr>
-	
-	<tr>
-			
-				<td on:click={() => remover2(tabela2)}>
-				{#each tabela2 as tabela2,i}{tabela2}<img src={link} alt='yes'>{/each} 
-				</td> 
-		</tr> 
-	
-	<tr>
-			
-				<td on:click={() => remover3(tabela3)}> 
-					{#each tabela3 as tabela3,i}{tabela3}<img src={link} alt='yes'>{/each} 
-				</td> 
-		</tr>
-	
-	
-	
-	
-</table>
-	
-<br/>
-<br/>
-<br/>
-<button on:click={() => botaojogador1()}>Jogador 1</button>
-<button on:click={() => botaojogador2()}>Jogador 2</button>
-<br>
-<br>
-<VoltarMenu/>
 
-	</body>
+		<tr>
+			<td on:click={() => remover1(tabela1)}>
+				{#each tabela1 as tabela1, i}{tabela1}<img
+						src={link}
+						alt="yes"
+					/>{/each}
+			</td>
+		</tr>
+
+		<tr>
+			<td on:click={() => remover2(tabela2)}>
+				{#each tabela2 as tabela2, i}{tabela2}<img
+						src={link}
+						alt="yes"
+					/>{/each}
+			</td>
+		</tr>
+
+		<tr>
+			<td on:click={() => remover3(tabela3)}>
+				{#each tabela3 as tabela3, i}{tabela3}<img
+						src={link}
+						alt="yes"
+					/>{/each}
+			</td>
+		</tr>
+	</table>
+
+	<br />
+	<br />
+	<br />
+	<button on:click={() => botaojogador1()}>Jogador 1</button>
+	<button on:click={() => botaojogador2()}>Jogador 2</button>
+	<br />
+	<br />
+	<VoltarMenu />
+</body>
